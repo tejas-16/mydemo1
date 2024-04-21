ddd04c6c5a9483ef7787b0edd65dfb7e0a9fb908
tejas-16 <74181918+tejas-16@users.noreply.github.com>
2024-04-21 18:06:05 +0530

    Update email.py

--- email.py
+++ email.py
@@ -1,20 +1,19 @@
 import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from email.header import Header
 
 sender_email = "tejasvinchurkar16@gmail.com"
 receiver_email = "tejasvinchurkar.scoe.comp@gmail.com"
 password = "password"
 
 msg = MIMEMultipart()
-msg['tejasvinchurkar16@gmail.com'] = sender_email
-msg['tejasvinchurkar.scoe.comp@gmail.com'] = receiver_email
-msg['Subject: Unix Fastack Sanity Checkbject'] = Header('Some Title', 'utf-8').encode()
+msg['From'] = sender_email
+msg['To'] = receiver_email
+msg['Subject'] = 'Some Title'
 
-body = 'Hello Team, /Please help me with the sanity check.'
+body = 'Hello Team, Please help me with the sanity check.'
 
-msg_content = MIMEText(body, 'Hello Team, /Please help me with the sanity check.', 'utf-8')
+msg_content = MIMEText(body, 'plain', 'utf-8')
 msg.attach(msg_content)
 
 # Create secure connection with server and send email
